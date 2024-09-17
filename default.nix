690878aca0373423f69cb86bb095a5a42da6ec96
raf <rraf@tuta.io>
2024-09-17 20:43:49 +0100

    update: switched back to pipewire but properly

--- default.nix
+++ default.nix
@@ -54,6 +54,42 @@
 
   sound.enable = true;
   services = {
+    pipewire = {
+      enable = true;
+      pulse.enable = true;
+      alsa = {
+        enable = true;
+        support32Bit = true;
+      };
+      extraConfig = {
+        pipewire."92-low-latency" = {
+          context.properties = {
+            default.clock.rate = 48000;
+            default.clock.quantum = 32;
+            default.clock.min-quantum = 32;
+            default.clock.max-quantum = 32;
+          };
+        };
+        pipewire-pulse."92-low-latency" = {
+          context.modules = [
+            {
+              name = "libpipewire-module-protocol-pulse";
+              args = {
+                pulse.min.req = "32/48000";
+                pulse.default.req = "32/48000";
+                pulse.max.req = "32/48000";
+                pulse.min.quantum = "32/48000";
+                pulse.max.quantum = "32/48000";
+              };
+            }
+          ];
+          stream.properties = {
+            node.latency = "32/48000";
+            resample.quality = 1;
+          };
+        };
+      };
+    };
     openssh.enable = true;
     fwupd.enable = true;
     udev.packages = with pkgs; [ game-devices-udev-rules ];
@@ -78,10 +114,6 @@
 
   hardware = {
     uinput.enable = true;
-    pulseaudio = {
-      enable = true;
-      extraConfig = "load-module module-switch-on-connect";
-    };
     bluetooth = {
       enable = true;
       powerOnBoot = true;
