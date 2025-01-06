3b48363366e52a562c102e0d55b3088916fef714
raf <rraf@tuta.io>
2025-01-06 15:41:50 +0000

    update: switched to pulseaudio because of pipewire bugs

--- sound.nix
+++ sound.nix
@@ -1,78 +1,12 @@
 { pkgs, ... }:
 
 {
-  hardware.firmware = [ pkgs.sof-firmware ];
-  services.pipewire = {
-    enable = true;
-    pulse.enable = true;
-    alsa = {
+  services.pipewire.enable = false;
+  hardware = {
+    firmware = [ pkgs.sof-firmware ];
+    pulseaudio = {
       enable = true;
-      support32Bit = true;
-    };
-    extraConfig = {
-      pipewire."93-detect-devices" = {
-        context.modules = [
-          { name = "libpipewire-module-alsa"; }
-          { name = "libpipewire-module-udev-detect"; }
-        ];
-      };
-      pipewire."92-low-latency" = {
-        context.properties = {
-          default.clock.rate = 48000;
-          default.clock.quantum = 32;
-          default.clock.min-quantum = 32;
-          default.clock.max-quantum = 32;
-        };
-      };
-      pipewire-pulse."92-low-latency" = {
-        context.modules = [
-          {
-            name = "libpipewire-module-protocol-pulse";
-            args = {
-              pulse.min.req = "32/48000";
-              pulse.default.req = "32/48000";
-              pulse.max.req = "32/48000";
-              pulse.min.quantum = "32/48000";
-              pulse.max.quantum = "32/48000";
-            };
-          }
-        ];
-        stream.properties = {
-          node.latency = "32/48000";
-          resample.quality = 1;
-        };
-      };
+      extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1";
     };
   };
-
-  environment.etc."wireplumber/wireplumber.conf.d/50-alsa-config.conf".text = ''
-    monitor.alsa.rules = [
-      {
-        matches = [
-          {
-            node.name = "~alsa_output.*"
-          }
-        ]
-        actions = {
-          update-props = {
-            api.alsa.period-size   = 1024
-            api.alsa.headroom      = 8192
-          }
-        }
-      }
-    ]
-  '';
-
-  nixpkgs.overlays = [
-    (self: super: {
-      audio = pkgs.writeScriptBin "audio" ''
-        #!${pkgs.stdenv.shell}
-        pulsemixer
-      '';
-    })
-  ];
-  environment.systemPackages = [
-    pkgs.pulsemixer
-    pkgs.audio
-  ];
 }
