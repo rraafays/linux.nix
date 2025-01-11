2c586ac4a8d45306bbbf901d5f4742b52b4b3b44
raf <rraf@tuta.io>
2025-01-11 16:19:40 +0000

    update: mpd pipewire

--- sound.nix
+++ sound.nix
@@ -1,12 +1,6 @@
 { pkgs, ... }:
 
 {
-  services.pipewire.enable = false;
-  hardware = {
-    firmware = [ pkgs.sof-firmware ];
-    pulseaudio = {
-      enable = true;
-      extraConfig = "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1";
-    };
-  };
+  services.pipewire.enable = true;
+  hardware.firmware = [ pkgs.sof-firmware ];
 }
