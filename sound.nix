3ed737d24b801936300e5fac75e7378d95c35fa9
raf <rraf@tuta.io>
2025-01-17 19:19:06 +0000

    fix: low latency was glitchy

--- sound.nix
+++ sound.nix
@@ -4,35 +4,6 @@
   hardware.firmware = [ pkgs.sof-firmware ];
   services.pipewire = {
     enable = true;
-    extraConfig = {
-      pipewire."92-low-latency" = {
-        "context.properties" = {
-          "default.clock.rate" = 48000;
-          "default.clock.quantum" = 32;
-          "default.clock.min-quantum" = 32;
-          "default.clock.max-quantum" = 32;
-        };
-      };
-      pipewire-pulse."92-low-latency" = {
-        "context.properties" = [
-          {
-            name = "libpipewire-module-protocol-pulse";
-            args = { };
-          }
-        ];
-        "pulse.properties" = {
-          "pulse.min.req" = "32/48000";
-          "pulse.default.req" = "32/48000";
-          "pulse.max.req" = "32/48000";
-          "pulse.min.quantum" = "32/48000";
-          "pulse.max.quantum" = "32/48000";
-        };
-        "stream.properties" = {
-          "node.latency" = "32/48000";
-          "resample.quality" = 1;
-        };
-      };
-    };
     wireplumber.extraConfig."99-disable-suspend" = {
       "monitor.alsa.rules" = [
         {
