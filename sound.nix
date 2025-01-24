ec4260191a795acaa6cc7d0b4c5d1b48e54a5d17
raf <rraf@tuta.io>
2025-01-24 14:07:31 +0000

    fix: increased quantum and sample rate for more stable audio

--- sound.nix
+++ sound.nix
@@ -4,6 +4,37 @@
   hardware.firmware = [ pkgs.sof-firmware ];
   services.pipewire = {
     enable = true;
+    extraConfig = {
+      pipewire."92-low-latency" = {
+        "context.properties" = {
+          "default.clock.rate" = 96000;
+          "default.clock.quantum" = 1024;
+          "default.clock.min-quantum" = 1024;
+          "default.clock.max-quantum" = 1024;
+        };
+      };
+
+      pipewire-pulse."92-low-latency" = {
+        "context.properties" = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = { };
+          }
+        ];
+        "pulse.properties" = {
+          "pulse.min.req" = "1024/96000";
+          "pulse.default.req" = "1024/96000";
+          "pulse.max.req" = "1024/96000";
+          "pulse.min.quantum" = "1024/96000";
+          "pulse.max.quantum" = "1024/96000";
+        };
+        "stream.properties" = {
+          "node.latency" = "1024/96000";
+          "resample.quality" = 1;
+        };
+      };
+    };
+
     wireplumber.extraConfig."99-disable-suspend" = {
       "monitor.alsa.rules" = [
         {
